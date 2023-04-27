40d6cb2e1317c4703cd80f7b2049b6138de3d383
lordish <gitonga403@gmail.com>
2023-04-27 07:18:56 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,25 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+    <Switch>
+      <Route exact path="/home">
+          <Home/>
+      </Route>
+      <Route path="navbar">
+          <NavBar/>
+      </Route>
+      <Route path="/actors">
+          <Actors/>
+      </Route>
+      <Route path="/directors">
+          <Directors/>
+      </Route>
+      <Route path="/movies">
+          <Movies/>
+      </Route>
+  </Switch>
+  </div>;
 }
 
 export default App;
